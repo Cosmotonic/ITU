9ecb150dbc94b05858717889a6083c44b58b52aa
kasperlarsson <contactkasperlarsson@gmail.com>
2026-04-02 09:24:54 +0200

    flights script

---
+++ Semester02/algorithmsanddatastructures/Week9/Flights.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Flights{
+
+    public static void main(String[] args) throws IOException  {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //(new FileReader("Week7\\input.in"));
+        String input = br.readLine(); 
+        System.out.println(input);
+        
+        
+
+    }
+
+
+
+
+
+}
