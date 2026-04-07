1a57e01c7422e25cb99f6f3dc8fd633d3d052621
kasperlarsson <contactkasperlarsson@gmail.com>
2026-04-07 18:48:14 +0200

    update

---
+++ Semester02/algorithmsanddatastructures/Week9/ExamTest.java
@@ -0,0 +1,13 @@
+public class ExamTest{
+
+
+    public static void main(String[] args) {
+        
+        int[] numbers = {1,2,3,4}; 
+        int[] numbers1 = {1,2,3}; 
+        System.out.println((numbers.length - 1)/2);
+        System.out.println(numbers[(numbers.length-1)/2]);
+        System.out.println(numbers.length);
+        // System.out.println(numbers1.length-1/2);
+    }
+}
